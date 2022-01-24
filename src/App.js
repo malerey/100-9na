520b73a8e278f399a856400e77fd9f5afa4910d1
malerey <reymalena@gmail.com>
2022-01-24 20:54:19 -0300

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// ESTADO - EVENTOS 
+// hook 
+import { useState } from "react";
+// variables de estado
+// indican el estado de la aplicacion
+// cuando se modifican, se modifica lo que ve el usuario
+// son variables globales
 
-function App() {
+const App = () => {
+
+  // cuando hay variables de estado, las debemos declarar como tales 
+  // cuando se modifica una variable cualquiera, el dom no cambia
+  // cuando se modifica una variable de estado el dom si cambia
+
+  // puedo tener muchos estados por componente
+  // los estados pueden ser un dato de cualquier tipo
+
+  // SI QUIERO QUE ALGO CAMBIE EN LA UI, NECESITO UNA VARIABLE DE ESTADO
+  const [ contador, setContador ] = useState(0)
+  const [nombre, setNombre ] = useState("Oliver")
+
+
+  const handleClickBotonMas = () => {
+    setContador(contador + 1)
+    setNombre("Gabi")
+  }
+
+  const handleClickBotonMenos = () => {
+    setContador(contador - 1)
+    setNombre("Oliver")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+    <p>El gato de Gri se llama {nombre}</p>
+    <p>Contador: <span>{contador}</span></p>
+      <button 
+          onClick={handleClickBotonMas} 
         >
-          Learn React
-        </a>
-      </header>
+          +1
+        </button>
+      <button
+        onClick={handleClickBotonMenos}
+      >-1</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
